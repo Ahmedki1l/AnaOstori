4e439b9c0284d3391515e7371e87bc6034bfa6a8
chirag <79736243+cmoradiya55@users.noreply.github.com>
2023-05-26 10:59:03 +0530

    Minor Bug resolved

--- components/antDesignCompo/CheckBox.js
+++ components/antDesignCompo/CheckBox.js
@@ -5,19 +5,39 @@ import styled from 'styled-components'
 
 
 const AntdCheckboxStyle = styled(AntdCheckbox)`
- 
-  .ant-checkbox-inner{
-    // background-color: #F26722;
-    // border-color: #F26722;
-    width: 26px;
-    height: 26px;
-    border:0.5px solid #0000008a;
-  }
-
-  .ant-checkbox-wrapper:not(.ant-checkbox-wrapper-disabled):hover .ant-checkbox-checked:not(.ant-checkbox-disabled) .ant-checkbox-inner {
-    background-color: #F26722;
-    border-color: #F26722;
-  }
+    input[type="checkbox"] {
+        width: 26px !important;
+        height: 26px !important;
+    }
+    .ant-checkbox-inner{
+        width: 26px;
+        height: 26px;
+        border:0.5px solid #0000008a;
+    }
+    .ant-checkbox-inner:after {
+        width: 7px;
+        height: 13px;
+        top: 47%;
+    }
+    .ant-checkbox-wrapper:hover .ant-checkbox-inner {
+        border-color: #0000008a !important;
+    }
+    .ant-checkbox:hover .ant-checkbox-inner {
+        border-color: #0000008a !important;
+    }
+    .ant-checkbox-checked .ant-checkbox-inner {
+        background-color: #F26722;
+        border-color: #F26722;
+    }   
+    .ant-checkbox-wrapper:hover .ant-checkbox-checked .ant-checkbox-inner {
+        border-color: #F26722 !important;
+        background-color: #F26722 !important;
+    }
+    .ant-checkbox:hover .ant-checkbox-checked > .ant-checkbox-inner {
+        border-color: #F26722 !important;
+        background-color: #F26722 !important;
+    }
+   
 `
 const CheckBoxLabel = styled('p')`
     font-size: 20px;
