0e676852a2618460f895492c0ac9a0cd5426fe39
cmoradiya55 <79736243+cmoradiya55@users.noreply.github.com>
2023-06-07 08:44:57 +0530

    appointment modal design

---
+++ components/antDesignCompo/Datepicker.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import styled from 'styled-components';
+import { DatePicker as AntdDatePicker } from "antd";
+import AllIconsComponenet from '../../Icons/AllIconsComponenet';
+
+const StyledDatePicker = styled(AntdDatePicker)`
+  height: ${props => (props.height ? props.height : '40')}px !important;
+  width: ${props => (props.width ? props.width : '172')}px !important;
+  align-items:center;
+
+  .ant-picker-input > input {
+    font-size: 16px;
+    font-family: 'Tajawal-Regular';
+    padding:0 20px;
+    margin-top: 4px;
+  }
+  .ant-picker-input > input::placeholder {
+    font-size: 16px;
+    font-family: 'Tajawal-Regular';
+    padding:5px;
+  }
+  .ant-picker-suffix{
+    position: absolute;
+    right:-6px;
+  }
+`;
+const onChange = (date, dateString) => {
+  console.log(date, dateString);
+};
+const DatePicker = ({
+  OptionData,
+  placeholder,
+  picker,
+  suFFixIconName,
+  ...rest
+}) => {
+  return (
+    <StyledDatePicker
+      onChange={onChange}
+      placeholder={placeholder}
+      picker={picker}
+      suffixIcon={<AllIconsComponenet height={16} width={16} iconName={suFFixIconName} color={'#00000080'} />}
+      {...rest}
+    />
+  );
+}
+
+export default DatePicker;
+
--- components/antDesignCompo/FormItem.js
+++ components/antDesignCompo/FormItem.js
@@ -1,6 +1,10 @@
 import { Form as AntdForm } from 'antd'
 import styled from 'styled-components'
 
+export const Form = styled(AntdForm)`
+  direction:rtl;
+`
+
 export const FormItem = styled(AntdForm.Item)`
   margin-bottom: ${props => props.marginbottom ? props.marginbottom : '20'}px  !important;
   margin-left: ${props => props.marginleft ? props.marginleft : '12'}px !important;
