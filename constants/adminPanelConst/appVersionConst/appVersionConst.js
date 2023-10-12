7ce002b64926f67df7035e8f92e3e46dc0f00b86
chirag <>
2023-10-12 18:59:56 +0530

    appVersionChanges

--- constants/adminPanelConst/appVersionConst/appVersionConst.js
+++ constants/adminPanelConst/appVersionConst/appVersionConst.js
@@ -4,8 +4,19 @@ export const appVersionConst = {
     minimumVersionText: 'رقم النسخة الأدنى*',
     androidVersionText: 'نسخة الاندرويد',
     currentVersionPlaceHolder: 'رقم نسخة الاندرويد الحالية',
+    currentAndroidVersionInputErorMsg: 'اكتب رقم نسخة الاندرويد الحالية',
+
     minimumVersionPlaceHolder: 'رقم نسخة الاندرويد الأدنى',
+    minimumVersionInputErrorMsg: 'اكتب رقم نسخة الاندرويد الأدنى',
+
     appleVersionText: 'نسخة الايفون',
+
+    iosCurrentVersionPlaceHolder: 'رقم نسخة الايفون الحالية',
+    iorCurrenVersionInputErrorMsg: 'اكتب رقم نسخة الايفون الحالية',
+
+    iosMinimunVersionPlaceHolder: 'رقم نسخة الايفون الأدنى',
+    iosMinimumVersionInputErrorMsg: 'اكتب رقم نسخة الايفون الأدنى',
+
     currurentVersionInputError: 'ادخل رقم النسخة الحالية',
     minimumVersionInputError: 'ادخل رقم النسخة الأدنى',
 }
