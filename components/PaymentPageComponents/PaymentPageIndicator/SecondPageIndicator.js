ab89ae8642938db2d1823d80f426f4ccc0f61997
chirag <79736243+cmoradiya55@users.noreply.github.com>
2024-02-08 18:50:08 +0530

    mostViewedPage hide

--- components/PaymentPageComponents/PaymentPageIndicator/SecondPageIndicator.js
+++ components/PaymentPageComponents/PaymentPageIndicator/SecondPageIndicator.js
@@ -11,14 +11,14 @@ export default function SecondPageIndicator() {
 				<div className={`${styles.circle} ${styles.firstPageBox}`}>
 					<AllIconsComponenet iconName={'checkCircleRoundIcon'} height={40} width={30} color={'#00B564'} />
 				</div>
-				<p className={`fontRegular ${styles.pageNoHead}`}>البيانات الشخصية</p>
+				<p className={`fontMedium ${styles.pageNoHead}`}>البيانات الشخصية</p>
 			</div>
 			<div className={`absolute ${styles.middleLine}`}></div>
 			<div className={`absolute ${styles.pageNoTwo}`}>
 				<div className={`${styles.circle} ${styles.secondPage}`}>
 					<p className="fontBold">2</p>
 				</div>
-				<p className={`fontBold ${styles.pageNoHead}`}>مراجعة الطلب والدفع</p>
+				<p className={`fontMedium ${styles.pageNoHead}`}>مراجعة الطلب والدفع</p>
 			</div>
 		</div>
 	)
