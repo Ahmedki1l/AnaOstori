c7251e7fce79b0427f86f4da411f7629c537ea26
ChiragUex <143417375+ChiragUex@users.noreply.github.com>
2024-09-02 19:28:33 +0530

    add dummy commit

--- pages/instructorPanel/index.js
+++ pages/instructorPanel/index.js
@@ -63,6 +63,7 @@ export default function Index() {
                     <AllIconsComponenet width={39} height={39} iconName='manageCoupons' color={'#000000'} />
                     <p className={`fontMedium ${styles.dashboardItemName}`}>إضافة وتعديل كوبونات الخصم</p>
                 </Link>
+                {/* this is used to add city */}
                 {/* <Link href={`/instructorPanel/manageBranches?region=all`} className={`${styles.dashboardItemWrapper} normalLinkText`}>
                     <AllIconsComponenet width={39} height={39} iconName='locationDoubleColor' color={'#000000'} />
                     <p className={`fontMedium ${styles.dashboardItemName}`}>Manage state and branch</p>
