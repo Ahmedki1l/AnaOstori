e01bad043f5a7c083bd925c6758898bc4f0360d0
Ahmedki1l <ahmedzzz456@gmail.com>
2025-03-16 17:59:09 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -194,7 +194,7 @@ function MyApp({ Component, pageProps }) {
 					<meta property="og:url" content={`https://anaostori.com${router.asPath}`} />
 					<meta property="og:type" content={`https://anaostori.com}`} />
 					<meta property="og:image" content={`${metaTags.image}`} />
-					<meta http-equiv="Content-Security-Policy"
+					{/* <meta http-equiv="Content-Security-Policy"
 						content="
 						style-src 'self' https://eu-test.oppwa.com 'unsafe-inline' ;
 						frame-src 'self' https://eu-test.oppwa.com;
@@ -202,7 +202,7 @@ function MyApp({ Component, pageProps }) {
 						connect-src 'self' https://eu-test.oppwa.com;
 						img-src 'self' https://eu-test.oppwa.com;
 						">
-					</meta>
+					</meta> */}
 				</Head>
 				<div dir='rtl'>
 					<GoogleAnalytics pathName={pathName} />
