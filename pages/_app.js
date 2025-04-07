345d51c2c5aea6ab0378c4a192624b3d3e0d366f
Ahmedki1l <ahmedzzz456@gmail.com>
2025-04-08 00:36:09 +0200

    adding google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -211,6 +211,54 @@ function MyApp({ Component, pageProps }) {
 					/> */}
 				</Head>
 				<div dir='rtl'>
+
+					{/* -------------------------------------------------------- */}
+					{/*  1) Google Analytics (gtag.js)  */}
+					{/* -------------------------------------------------------- */}
+					<Script
+						src="https://www.googletagmanager.com/gtag/js?id=G-D11BLBX9E0"
+						strategy="afterInteractive"
+					/>
+					<Script
+						id="google-analytics"
+						strategy="afterInteractive"
+								>{`
+						window.dataLayer = window.dataLayer || [];
+						function gtag(){dataLayer.push(arguments);}
+						gtag('js', new Date());
+						gtag('config', 'G-D11BLBX9E0');
+					`}</Script>
+
+					{/* -------------------------------------------------------- */}
+					{/*  2) Google Tag Manager  */}
+					{/* -------------------------------------------------------- */}
+					<Script
+						id="google-tag-manager"
+						strategy="afterInteractive"
+						dangerouslySetInnerHTML={{
+										__html: `
+							(function(w,d,s,l,i){w[l]=w[l]||[];
+							w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
+							var f=d.getElementsByTagName(s)[0],
+								j=d.createElement(s),
+								dl=l!='dataLayer'?'&l='+l:'';
+							j.async=true; j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+							f.parentNode.insertBefore(j,f);
+							})(window,document,'script','dataLayer','GTM-TKLKW2N8');
+						`,
+						}}
+					/>
+
+					{/* Optional GTM noscript fallback (usually placed after <body>) */}
+					<noscript>
+						<iframe
+							src="https://www.googletagmanager.com/ns.html?id=GTM-TKLKW2N8"
+							height="0"
+							width="0"
+							style={{ display: 'none', visibility: 'hidden' }}
+						></iframe>
+					</noscript>
+
 					<GoogleAnalytics pathName={pathName} />
 					<Script
 						id="fb-pixel"
