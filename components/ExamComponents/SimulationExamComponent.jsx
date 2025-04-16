00271d05d94f514bb4dd9a90bb1b1baff95a60d0
Ahmedki1l <ahmedzzz456@gmail.com>
2025-04-16 23:40:49 +0200

    Update SimulationExamComponent.jsx

--- components/ExamComponents/SimulationExamComponent.jsx
+++ components/ExamComponents/SimulationExamComponent.jsx
@@ -365,7 +365,7 @@ const SimulationExamComponent = ({
                                                 {/* Copy Link (only for exams, not folders) */}
                                                 {typeOfListdata === 'exam' && (
                                                     <div onClick={() => handleCopyLink(item._id)}>
-                                                        <AllIconsComponenet iconName='copy' height={24} width={24} />
+                                                        <AllIconsComponenet iconName='copy' height={24} width={24} color={'#000000'} />
                                                     </div>
                                                 )}
                                                 <div onClick={() => handleDeleteFolderItems(item)}>
