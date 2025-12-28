cf8e1e383e6416efbc2eee3323fdae2ebf3fc9e3
ahmed alaa <ahmedzzz456@gmail.com>
2025-12-28 16:21:17 +0200

    feat: Initialize Next.js application with global layout, Redux, React Query, and various analytics and pixel integrations.

--- pages/_app.js
+++ pages/_app.js
@@ -327,14 +327,13 @@ function MyApp({ Component, pageProps }) {
 						dangerouslySetInnerHTML={{
 							__html: `
 						!function (w, d, t) {
-						w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];
-						ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],
-						ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};
-						for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);
-						ttq.instance=function(t){for(var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},
-						ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script");n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
-						ttq.load('${tiktok.TIKTOK_PIXEL_ID}');
-						ttq.page();
+						  w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(
+						var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script")
+						;n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
+
+
+						  ttq.load('CO7I57JC77UCLVHEH8N0');
+						  ttq.page();
 						}(window, document, 'ttq');
 						`,
 						}}
@@ -350,9 +349,11 @@ function MyApp({ Component, pageProps }) {
 						r.src=n;var u=t.getElementsByTagName(s)[0];
 						u.parentNode.insertBefore(r,u);})(window,document,
 						'https://sc-static.net/scevent.min.js');
-						snaptr('init','${process.env.NEXT_PUBLIC_SNAPCHAT_PIXEL_ID}', {
-						'user_email': '__INSERT_USER_EMAIL__'
+
+						snaptr('init', '662c5563-9431-4706-a60f-0a6aa3d3d111', {
+						'user_email': '_INSERT_USER_EMAIL_'
 						});
+
 						snaptr('track', 'PAGE_VIEW');
 						`,
 						}}
